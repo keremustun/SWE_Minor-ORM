733eeb3c2f4b8f2cae188fcd20280f563cb73d3c
fatih <0986192@hr.nl>
2022-03-09 11:21:10 +0100

    new database

--- LINQ2SQL/Program.cs
+++ LINQ2SQL/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
-using System.Data.SqlClient;
+using Npgsql;
 
 namespace LINQ2SQL
 {
     class Program
     {
-        public static string connection_string = @"Data source=(localdb)\MSSQLLocalDB;Initial Catalog=LINQ2SQL;Integrated Security = True";
+        public static string connection_string = "UserID=postgres;Password=root;Host=localhost;Port=5432;Database=LINQ2SQL;Pooling=true;";
+        public static string students = "CREATE TABLE students (name VARCHAR ( 50 ) NOT NULL, surname VARCHAR ( 50 ) NOT NULL);";
+        public static string grades = "CREATE TABLE grades (value VARCHAR ( 50 ) NOT NULL, courseid int NOT NULL);";
 
         static void Main(string[] args)
         {
@@ -15,17 +17,33 @@ namespace LINQ2SQL
 
         public static void GetStudents()
         {
-            using (SqlConnection con = new(connection_string))
+            /*using (var con = new NpgsqlConnection(connection_string))
             {
                 con.Open();
-                SqlCommand cmd = new("SELECT * FROM Students WHERE Name = 'Emir'", con);
-                using (SqlDataReader reader = cmd.ExecuteReader())
-                {
-                    reader.Read();
-                    Console.WriteLine(reader.GetValue(0));
+                var cmd = new NpgsqlCommand("CREATE TABLE Students", con);
+                cmd.ExecuteNonQuery();
+            }
 
+            var connString = "Host=myserver;Username=mylogin;Password=mypass;Database=mydatabase";*/
+
+            var conn = new NpgsqlConnection(connection_string);
+            conn.Open();
+
+            // Insert some data
+            /*using (var cmd = new NpgsqlCommand(students, conn))
+            {
+                cmd.ExecuteNonQuery();
+                Console.WriteLine('k');
+            }*/
+
+            // Retrieve all rows
+            using (var cmd = new NpgsqlCommand("SELECT * FROM students", conn))
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read()) {
+                    Console.WriteLine(reader.GetString(0));
+                    Console.WriteLine(reader.GetString(1));
                 }
-
             }
         }
     }
