3ddb0dde8097543656bb3937f45b5eb6b14df23c
fatih <0986192@hr.nl>
2022-03-09 11:32:13 +0100

    data

--- LINQ2SQL/Program.cs
+++ LINQ2SQL/Program.cs
@@ -6,8 +6,8 @@ namespace LINQ2SQL
     class Program
     {
         public static string connection_string = "UserID=postgres;Password=root;Host=localhost;Port=5432;Database=LINQ2SQL;Pooling=true;";
-        public static string students = "CREATE TABLE students (name VARCHAR ( 50 ) NOT NULL, surname VARCHAR ( 50 ) NOT NULL);";
-        public static string grades = "CREATE TABLE grades (value VARCHAR ( 50 ) NOT NULL, courseid int NOT NULL);";
+        public static string students = "CREATE TABLE IF NOT EXISTS students (name VARCHAR ( 50 ) NOT NULL, surname VARCHAR ( 50 ) NOT NULL);";
+        public static string grades = "CREATE TABLE IF NOT EXISTS grades (value VARCHAR ( 50 ) NOT NULL, courseid int NOT NULL);";
 
         static void Main(string[] args)
         {
@@ -17,24 +17,21 @@ namespace LINQ2SQL
 
         public static void GetStudents()
         {
-            /*using (var con = new NpgsqlConnection(connection_string))
-            {
-                con.Open();
-                var cmd = new NpgsqlCommand("CREATE TABLE Students", con);
-                cmd.ExecuteNonQuery();
-            }
-
-            var connString = "Host=myserver;Username=mylogin;Password=mypass;Database=mydatabase";*/
-
             var conn = new NpgsqlConnection(connection_string);
             conn.Open();
 
             // Insert some data
-            /*using (var cmd = new NpgsqlCommand(students, conn))
+            using (var cmd = new NpgsqlCommand(students, conn))
             {
                 cmd.ExecuteNonQuery();
                 Console.WriteLine('k');
-            }*/
+            }
+
+            using (var cmd = new NpgsqlCommand(grades, conn))
+            {
+                cmd.ExecuteNonQuery();
+                Console.WriteLine('k');
+            }
 
             // Retrieve all rows
             using (var cmd = new NpgsqlCommand("SELECT * FROM students", conn))
